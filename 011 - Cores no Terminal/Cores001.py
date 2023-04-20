fbb9fa13de9d68f330213176d56ccf901d698f76
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2023-04-20 10:36:49 -0300

    Cores (desatualizado)

---
+++ 011 - Cores no Terminal/Cores001.py
@@ -0,0 +1,12 @@
+print('\033[1;31;43mOlá, Mundo!\033[m')
+print('\033[0;33;44mOla, Mundo!\33[m')
+nome = 'Usuario'
+cores = {'limpa':'\033[m}', 
+         'azul':'\033[34m', 
+         'amarelo':'\033[33m', 
+         'pretoebranco':'\033[7;30m'}
+print(f'Bom dia {nome}')
+print(f'Ola \033[4;34m{nome}\033[m  Muito prazer em te conhecer')
+a = 3
+b = 5
+print(f'Os valores sao \033[32m{a}\033[m e \033[31m{b}\033[m')
