496e6744c58d77ece29eff45efff3f83a1ce3af0
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2023-06-05 14:02:52 -0300

    Python First Website

--- 001 - Python First Website/main.py
+++ 001 - Python First Website/main.py
@@ -2,5 +2,13 @@ from website import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    # Quick test configuration. Please use proper Flask configuration options
+    # in production settings, and use a separate file or environment variables
+    # to manage the secret key!
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+
+
+    app.debug = True
+    app.run()
