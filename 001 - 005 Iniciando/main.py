763582632347e48d6ea6672fd8a2d6ec8dcdbf11
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2023-06-08 01:53:49 -0300

    Python Básico

---
+++ 001 - 005 Iniciando/main.py
@@ -0,0 +1,14 @@
+# nome = input("Qual seu nome? ")
+
+# print(f" Ola {nome} ! Prazer em te conhecer!")
+
+# dia = input("Dia: ")
+# mes = input("Mes: ")
+# ano = input("Ano: ")
+
+# print(f"Voce nasceu no dia {dia} no mes de {mes} no ano de {ano}")
+
+n1 = int(input("Digite um numero: "))
+n2 = int(input("Digite outro numero: "))
+s = n1 + n2
+print(f"A soma de {n1} e {n2} é {s}")
