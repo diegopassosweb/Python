2edc4bc0c0ad8e0a86f6c904301aa7ad8f1e82cd
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2023-06-13 02:15:53 -0300

    Python

---
+++ Python Básico/024.py
@@ -0,0 +1,2 @@
+cid = str(input("Em que cidade nasceu? ")).strip()
+print(cid[0:5].upper() == "SANTO")
---
+++ Python Básico/main9.py
@@ -0,0 +1,10 @@
+
+frase = "Bom dia flores do dia"
+print(frase.upper().count('O'))
+print(len(frase))
+print(frase.strip())
+print(frase.replace("flores", "python"))
+print("dia" in frase)
+print(frase.lower().find("do"))
+print(frase.split())
+print(frase[1])
