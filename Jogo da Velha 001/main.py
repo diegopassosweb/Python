386beb7e853534dccb1bb9b4cf7f1ceb08409f5f
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2023-09-21 02:24:30 -0300

    Update

---
+++ Jogo da Velha 001/main.py
@@ -0,0 +1,70 @@
+import tkinter
+from tkinter import *
+from tkinter import ttk
+
+co0 = "#FFFFFF"  # branca / white
+co1 = "#333333"  # branca
+co2 = "#fcc058"  # laranja / orange
+co3 = "#38576b"  # valor / value
+co4 = "#3297a8"   # azul / blue
+co5 = "#fff873"   # amarela / yellow
+co6 = "#fcc058"  # laranja / orange
+co7 = "#e85151"   # vermelha / red
+co8 = co4   # + verde
+co10 ="#fcfbf7"
+fundo = "#3b3b3b" # preta / black
+
+janela = Tk()
+janela.title('')
+janela.geometry('260x370')
+janela.configure(bg=fundo)
+
+#dividindo a janela em 2 frames
+
+frame_cima = Frame(janela, width=240, height=100, bg=co1, relief="raised")
+frame_cima.grid(row=0, column=0, sticky=NW, padx=10, pady=10)
+
+frame_baixo = Frame(janela, width=240, height=300, bg=fundo, relief="flat")
+frame_baixo.grid(row=1, column=0, sticky=NW)
+
+# configurando o frame cima
+app_x = Label(frame_cima, text='X', height=1, relief="flat", anchor="center", font=('Ivy 40 bold'), bg=co1, fg=co7)
+app_x.place(x=25, y=10)
+app_x = Label(frame_cima, text='Jogador 1', height=1, relief="flat", anchor="center", font=('Ivy 7 bold'), bg=co1, fg=co7 )
+app_x.place(x=17, y=70)
+app_x_pontos = Label(frame_cima, text='0', height=1, relief="flat", anchor="center", font=('Ivy 30 bold'), bg=co1, fg=co0)
+app_x_pontos.place(x=80, y=20)
+
+app_separador = Label(frame_cima, text=':', height=1, relief="flat", anchor="center", font=('Ivy 30 bold'), bg=co1, fg=co0)
+app_separador.place(x=110, y=20)
+
+app_o = Label(frame_cima, text='O', height=1, relief="flat", anchor="center", font=('Ivy 40 bold'), bg=co1, fg=co4)
+app_o.place(x=170, y=10)
+app_o = Label(frame_cima, text='Jogador 2', height=1, relief="flat", anchor="center", font=('Ivy 7 bold'), bg=co1, fg=co7)
+app_o.place(x=165, y=70)
+app_o_pontos = Label(frame_cima, text='0', height=1, relief="flat", anchor="center", font=('Ivy 30 bold'), bg=co1, fg=co0)
+app_o_pontos.place(x=130, y=20)
+
+#configurando frame baixo
+
+# linhas verticais
+app_ = Label(frame_baixo, text='', height=23, relief="flat", pady=5, anchor="center", font=('Ivy 5 bold'), bg=co0, fg=co0)
+app_.place(x=90, y=15)
+app_ = Label(frame_baixo, text='', height=23, relief="flat", pady=5, anchor="center", font=('Ivy 5 bold'), bg=co0, fg=co0)
+app_.place(x=160, y=15)
+
+#linhas horizontais
+app_ = Label(frame_baixo, text=' ', width=190, pady=1, relief="flat", padx=2, anchor="center", font=('Ivy 1 bold'), bg=co0, fg=co0)
+app_.place(x=30, y=63)
+app_ = Label(frame_baixo, text=' ', width=190, pady=1, relief="flat", padx=2, anchor="center", font=('Ivy 1 bold'), bg=co0, fg=co0)
+app_.place(x=30, y=123)
+
+#linha 0
+b_0 = Label(frame_baixo, text=' ', width=190, pady=1, relief="flat", padx=2, anchor="center", font=('Ivy 1 bold'), bg=co0, fg=co0)
+b_0.place(x=30, y=63)
+
+
+
+
+
+janela.mainloop()
